4ed246a83d52adb6f711a3af3173866c7825fb5b
Gabriel Dota <gabrieldotafujita1234@hotmail.com>
2022-11-09 20:47:35 -0300

    feat: Cria a seção de perguntas e respostas

---
+++ src/components/GameScreen.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class GameScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      questions: [],
+      number: 0,
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { history } = this.props;
+    const token = localStorage.getItem('token');
+    const URL = `https://opentdb.com/api.php?amount=5&token=${token}`;
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+    const errorCode = 3;
+    if (data.response_code === errorCode) {
+      localStorage.removeItem('token');
+      history.push('/');
+    }
+    this.setState({
+      questions: data.results,
+      loading: false,
+    });
+  }
+
+  // Fisher–Yates shuffle
+  randomizeQuestions = (...questions) => {
+    const result = [];
+    let originalArray = questions.map((question, i) => ({
+      testId: i === 0 ? 'correct-answer' : `wrong-answer-${i - 1}`,
+      question,
+    }));
+    while (originalArray.length > 0) {
+      const index = Math.floor(Math.random() * originalArray.length);
+      result.push(originalArray[index]);
+      originalArray = originalArray.filter((_elements, i) => i !== index);
+    }
+    return result;
+  };
+
+  render() {
+    const { questions, number, loading } = this.state;
+    console.log(questions);
+    if (loading) {
+      return (<div>...Loading</div>);
+    }
+    const respostas = this.randomizeQuestions(
+      questions[number].correct_answer,
+      ...questions[number].incorrect_answers,
+
+    );
+    return (
+      <div>
+        <p
+          data-testid="question-category"
+        >
+          {questions[number].category}
+        </p>
+        <p
+          data-testid="question-text"
+        >
+          {questions[number].question}
+        </p>
+        <div data-testid="answer-options">
+          {respostas.map(({ testId, question }, i) => (
+            <button
+              key={ `Q${i}` }
+              type="button"
+              data-testid={ testId }
+              onClick={ () => {} }
+            >
+              {question}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+GameScreen.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default GameScreen;
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import GameScreen from '../components/GameScreen';
 
 class Game extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <Header />
+        <GameScreen history={ history } />
       </div>
     );
   }
 }
+
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
 export default Game;
