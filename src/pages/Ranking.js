59557373d1f5e292ef2292beeb9abf6751acee80
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-11-10 16:37:26 -0300

    feat: ordena ranking e exibe resultados na pagina

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
+// Ajustar ao final
+const rankingsMock = [
+  { name: 'player1', score: 100, picture: '4675ee57486c6ab9507d64d763ffd4f3' },
+  { name: 'player2', score: 40, picture: '4675ee57486c6ab9507d64d763ffd4f3' },
+  { name: 'player3', score: 80, picture: '4675ee57486c6ab9507d64d763ffd4f3' },
+  { name: 'player4', score: 50, picture: '4675ee57486c6ab9507d64d763ffd4f3' },
+];
+
 class Ranking extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      ranking: [],
+    };
+  }
+
+  componentDidMount() {
+    // const storedRanking = JSON.parse(localStorage.getItem('ranking'));
+    const ranking = rankingsMock.sort((a, b) => b.score - a.score);
+    this.setState({ ranking });
   }
 
   backHome = () => {
@@ -14,6 +30,8 @@ class Ranking extends React.Component {
   };
 
   render() {
+    const { ranking } = this.state;
+
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
@@ -24,6 +42,25 @@ class Ranking extends React.Component {
         >
           Início
         </button>
+        <section>
+          <ol>
+            {
+              ranking.map((player, index) => (
+                <li key={ `i${index}` }>
+                  <div>
+                    <img src={ `https://www.gravatar.com/avatar/${player.picture}` } alt="avatar" />
+                    <span data-testid={ `player-name-${index}` }>
+                      {`-${player.name}-`}
+                    </span>
+                    <span data-testid={ `player-score-${index}` }>
+                      {player.score}
+                    </span>
+                  </div>
+                </li>
+              ))
+            }
+          </ol>
+        </section>
       </div>
     );
   }
