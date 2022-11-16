13f90a8f9d9e0de10e6f214c9530596f045bc460
Gabriel Dota <gabrieldotafujita1234@hotmail.com>
2022-11-16 19:56:38 -0300

    fix: Testes do ranking

--- src/tests/Ranking.test.js
+++ src/tests/Ranking.test.js
@@ -53,12 +53,11 @@ describe('Testes relacionados à tela de ranking', () => {
   test('Verifica se a tela de ranking renderiza os elementos corretos', () => {
     localStorage.setItem('ranking', JSON.stringify([{name: "Gabriel Dota Fujita", score: 87, picture: "ce11fce876c93ed5d2a72da660496473"}]))
     renderWithRouterAndRedux(<Ranking />);
-    expect(screen.getByRole('button')).toHaveTextContent(/início/i);
+    expect(screen.getByRole('button')).toHaveTextContent(/jogar novamente/i);
     expect(screen.getByAltText('avatar')).toHaveAttribute('src', 'https://www.gravatar.com/avatar/ce11fce876c93ed5d2a72da660496473')
-    screen.logTestingPlaygroundURL();
   });
-  test('Verifica se a tela de ranking renderiza os players por ordem de pontos', async () => {
-    const { history } = renderWithRouterAndRedux(<App />);
+  test('Verifica se a aplicação consegue salvar dois perfis', async () => {
+    renderWithRouterAndRedux(<App />);
 
     jest.spyOn(global, 'fetch');
     global.fetch.mockResolvedValue({
@@ -81,6 +80,13 @@ describe('Testes relacionados à tela de ranking', () => {
     const perfis = JSON.parse(localStorage.getItem('ranking'));
     expect(perfis).toHaveLength(2);
   });
+  test('Verifica se o ranking renderiza os perfis em ordem de pontos',() => {
+    localStorage.setItem('ranking', '[{"name":"Bruno Pinheiro","score":136,"picture":"ee46a962466c5060acf9ef467a9b0bd7"},{"name":"teste","score":58,"picture":"4675ee57486c6ab9507d64d763ffd4f3"},{"name":"asdasd","score":120,"picture":"4f7818f30719e6a65194284a0c58050e"},{"name":"Bruno","score":90,"picture":"4f7818f30719e6a65194284a0c58050e"}]' )
+    renderWithRouterAndRedux(<Ranking />);
+    expect(screen.getByTestId('player-score-0')).toHaveTextContent(136)
+    expect(screen.getByTestId('player-score-1')).toHaveTextContent(120)
+    expect(screen.getByTestId('player-score-2')).toHaveTextContent(90)
+  })
   test('Verifica se o botão de inicio, renderiza o componente certo', () => {
     localStorage.setItem('ranking', JSON.stringify([{name: "Gabriel Dota Fujita", score: 87, picture: "ce11fce876c93ed5d2a72da660496473"}]))
     const { history } = renderWithRouterAndRedux(<App />, initialState);
